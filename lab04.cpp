ded6e75e7ea8c5dceb74a655aed5aa236b6dda02
Alyona <alyonacherepanova6954@gmail.com>
2021-12-17 17:33:53 +0300

    add standart work (threads = 0)

--- lab04.cpp
+++ lab04.cpp
@@ -13,6 +13,7 @@ using namespace std;
 #define KIND static
 
 int flows;
+const int SZ = 256;
 
 struct RGB {
     unsigned char r;
@@ -66,44 +67,65 @@ void treatment_P5 (FILE* fin, FILE* fout) {
 
     read_P5 (fin, fout, h, w, max_color, pixels);
 
-    vector <unsigned char> mns(flows, 255);
-    vector <unsigned char> mxs(flows);
-
     unsigned char mn = 255;
     unsigned char mx = 0;
-
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    omp_set_num_threads(flows);
-    #pragma omp parallel
-    {
-        #pragma omp for schedule(KIND, CHUNK_SIZE)
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                mns[omp_get_thread_num()] = min(mns[omp_get_thread_num()], pixels[i][j]);
-                mxs[omp_get_thread_num()] = max(mxs[omp_get_thread_num()], pixels[i][j]);
-            }
-        }
-    }
-
-    for (int flow = 0; flow < flows; flow++) {
-        mn = min(mn, mns[flow]);
-        mx = max(mx, mxs[flow]);
-    }
 
     auto f = [&] (unsigned char x) {
         return (unsigned char) (int(x - mn) * 255 / int(mx - mn));
     };
 
-    omp_set_num_threads(flows);
-    #pragma omp parallel
-    {
-        #pragma omp for schedule(KIND, CHUNK_SIZE)
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    
+    if (flows == 0) {
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                mn = min(mn, pixels[i][j]);
+                mx = max(mx, pixels[i][j]);
+            }
+        }
+
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
                 pixels[i][j] = f(pixels[i][j]);
             }
         }
+
     }
+    else {
+
+        vector <unsigned char> mns(flows, 255);
+        vector <unsigned char> mxs(flows);
+
+        omp_set_num_threads(flows);
+        #pragma omp parallel
+        {
+            #pragma omp for schedule(KIND, CHUNK_SIZE)
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    mns[omp_get_thread_num()] = min(mns[omp_get_thread_num()], pixels[i][j]);
+                    mxs[omp_get_thread_num()] = max(mxs[omp_get_thread_num()], pixels[i][j]);
+                }
+            }
+        }
+
+        for (int flow = 0; flow < flows; flow++) {
+            mn = min(mn, mns[flow]);
+            mx = max(mx, mxs[flow]);
+        }
+
+        omp_set_num_threads(flows);
+        #pragma omp parallel
+        {
+            #pragma omp for schedule(KIND, CHUNK_SIZE)
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    pixels[i][j] = f(pixels[i][j]);
+                }
+            }
+        }
+    }
+
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     cerr << "Time (" << flows << " thread(s)): ";
     cerr << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << " ms\n";
@@ -118,52 +140,79 @@ void treatment_P5 (FILE* fin, FILE* fout) {
 }
 
 void treatment_P6 (FILE* fin, FILE* fout) {
+    
     int h, w, max_color;
     vector <vector <RGB>> pixels;
     
     read_P6 (fin, fout, h, w, max_color, pixels);
 
-    vector <unsigned char> mns(flows, 255);
-    vector <unsigned char> mxs(flows);
-
     unsigned char mn = 255;
     unsigned char mx = 0;
-
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    omp_set_num_threads(flows);
-    #pragma omp parallel
-    {
-        #pragma omp for schedule(KIND, CHUNK_SIZE)
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                mns[omp_get_thread_num()] = min(mns[omp_get_thread_num()], pixels[i][j].r);
-                mns[omp_get_thread_num()] = min(mns[omp_get_thread_num()], pixels[i][j].g);
-                mns[omp_get_thread_num()] = min(mns[omp_get_thread_num()], pixels[i][j].b);
-                mxs[omp_get_thread_num()] = max(mxs[omp_get_thread_num()], pixels[i][j].r);
-                mxs[omp_get_thread_num()] = max(mxs[omp_get_thread_num()], pixels[i][j].g);
-                mxs[omp_get_thread_num()] = max(mxs[omp_get_thread_num()], pixels[i][j].b);
-            }
-        }
-    }
-
-    for (int flow = 0; flow < flows; flow++) {
-        mn = min(mn, mns[flow]);
-        mx = max(mx, mxs[flow]);
-    }
 
     auto f = [&] (unsigned char x) {
         return (unsigned char) (int(x - mn) * 255 / int(mx - mn));
     };
 
-    omp_set_num_threads(flows);
-    #pragma omp parallel
-    {
-        #pragma omp for schedule(KIND, CHUNK_SIZE)
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+    if (flows == 0) {
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                mn = min(mn, pixels[i][j].r);
+                mn = min(mn, pixels[i][j].g);
+                mn = min(mn, pixels[i][j].b);
+                mx = max(mx, pixels[i][j].r);
+                mx = max(mx, pixels[i][j].g);
+                mx = max(mx, pixels[i][j].b);
+            }
+        }
+
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
                 pixels[i][j].r = f(pixels[i][j].r);
                 pixels[i][j].g = f(pixels[i][j].g);
                 pixels[i][j].b = f(pixels[i][j].b);
+            }
+        }
+
+    }
+    else {
+
+        vector <unsigned char> mns(flows, 255);
+        vector <unsigned char> mxs(flows);
+
+        omp_set_num_threads(flows);
+        #pragma omp parallel
+        {
+            #pragma omp for schedule(KIND, CHUNK_SIZE)
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    mns[omp_get_thread_num()] = min(mns[omp_get_thread_num()], pixels[i][j].r);
+                    mns[omp_get_thread_num()] = min(mns[omp_get_thread_num()], pixels[i][j].g);
+                    mns[omp_get_thread_num()] = min(mns[omp_get_thread_num()], pixels[i][j].b);
+                    mxs[omp_get_thread_num()] = max(mxs[omp_get_thread_num()], pixels[i][j].r);
+                    mxs[omp_get_thread_num()] = max(mxs[omp_get_thread_num()], pixels[i][j].g);
+                    mxs[omp_get_thread_num()] = max(mxs[omp_get_thread_num()], pixels[i][j].b);
+                }
+            }
+        }
+
+        for (int flow = 0; flow < flows; flow++) {
+            mn = min(mn, mns[flow]);
+            mx = max(mx, mxs[flow]);
+        }
+
+        omp_set_num_threads(flows);
+        #pragma omp parallel
+        {
+            #pragma omp for schedule(KIND, CHUNK_SIZE)
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    pixels[i][j].r = f(pixels[i][j].r);
+                    pixels[i][j].g = f(pixels[i][j].g);
+                    pixels[i][j].b = f(pixels[i][j].b);
+                }
             }
         }
     }
@@ -185,48 +234,15 @@ void treatment_P6 (FILE* fin, FILE* fout) {
 void plus_P5 (FILE* fin, FILE* fout, double k) {
 
     int h, w, max_color;
-    vector <vector <unsigned char>> picsels;
+    vector <vector <unsigned char>> pixels;
 
-    read_P5 (fin, fout, h, w, max_color, picsels);
-
-    vector <vector <int>> srt (flows, vector <int> (255));
+    read_P5 (fin, fout, h, w, max_color, pixels);
 
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    omp_set_num_threads(flows);
-    #pragma omp parallel
-    {
-        #pragma omp for schedule(KIND, CHUNK_SIZE)
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) srt[omp_get_thread_num()][picsels[i][j]]++;
-        }
-    }
-
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-
-    cerr << "Time (" << flows << " thread(s)): ";
-    cerr << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << " ms\n";
 
     int sz = h * w;
     unsigned char mn = 255;
     unsigned char mx = 0;
-    
-    int pxls = 0;
-    for (int i = 0; i <= 255; i++) {
-        for (int flow = 0; flow < flows; flow++) pxls += srt[flow][i];
-        if ((double) pxls > (double) sz * k) {
-            mn = i;
-            break;
-        }
-    }
-
-    pxls = 0;
-    for (int i = 255; i >= 0; i--) {
-        for (int flow = 0; flow < flows; flow++) pxls += srt[flow][i];
-        if ((double) pxls > (double) sz * k) {
-            mx = i;
-            break;
-        }
-    }
 
     auto f = [&] (unsigned char x) {
         int pxl = (int(x - mn) * 255 / int(mx - mn));
@@ -235,11 +251,93 @@ void plus_P5 (FILE* fin, FILE* fout, double k) {
         return (unsigned char)(int(x - mn) * 255 / int(mx - mn));
     };
 
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
-            fprintf(fout, "%c", f(picsels[i][j]));
+
+    if (flows == 0) {
+
+        vector <int> srt (SZ);
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) srt[pixels[i][j]]++;
+        }
+
+        int pxls = 0;
+        for (int i = 0; i <= 255; i++) {
+            pxls += srt[i];
+            if ((double) pxls > (double) sz * k) {
+                mn = i;
+                break;
+            }
+        }
+
+        pxls = 0;
+        for (int i = 255; i >= 0; i--) {
+            pxls += srt[i];
+            if ((double) pxls > (double) sz * k) {
+                mx = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                pixels[i][j] = f(pixels[i][j]);
+            }
+        }
+
+    }
+    else {
+
+        vector <vector <int>> srt (flows, vector <int> (SZ));
+
+        omp_set_num_threads(flows);
+        #pragma omp parallel
+        {
+            #pragma omp for schedule(KIND, CHUNK_SIZE)
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) srt[omp_get_thread_num()][pixels[i][j]]++;
+            }
+        }
+        
+        int pxls = 0;
+        for (int i = 0; i <= 255; i++) {
+            for (int flow = 0; flow < flows; flow++) pxls += srt[flow][i];
+            if ((double) pxls > (double) sz * k) {
+                mn = i;
+                break;
+            }
+        }
+
+        pxls = 0;
+        for (int i = 255; i >= 0; i--) {
+            for (int flow = 0; flow < flows; flow++) pxls += srt[flow][i];
+            if ((double) pxls > (double) sz * k) {
+                mx = i;
+                break;
+            }
+        }
+
+        omp_set_num_threads(flows);
+        #pragma omp parallel
+        {
+            #pragma omp for schedule(KIND, CHUNK_SIZE)
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    pixels[i][j] = f(pixels[i][j]);
+                }
+            }
         }
     }
+    
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    cerr << "Time (" << flows << " thread(s)): ";
+    cerr << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << " ms\n";
+
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            fprintf(fout, "%c", pixels[i][j]);
+        }
+    }
+
 }
 
 void plus_P6 (FILE* fin, FILE* fout, double k) {
@@ -249,18 +347,108 @@ void plus_P6 (FILE* fin, FILE* fout, double k) {
 
     read_P6 (fin, fout, h, w, max_color, pixels);
 
-    vector <vector <int>> srt (flows, vector <int> (255));
-
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    omp_set_num_threads(flows);
-    #pragma omp parallel
-    {
-        #pragma omp for schedule(KIND, CHUNK_SIZE)
+
+    int sz = 3 * h * w;
+    unsigned char mn = 255;
+    unsigned char mx = 0;
+
+    auto f = [&] (unsigned char x) {
+        int pxl = (int(x - mn) * 255 / int(mx - mn));
+        if (pxl < 0) return (unsigned char) 0;
+        if (pxl > 255) return (unsigned char) 255;
+        return (unsigned char)(int(x - mn) * 255 / int(mx - mn));
+    };
+
+    if (flows == 0) {
+        
+        vector <int> srt (SZ);
+
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
-                srt[omp_get_thread_num()][pixels[i][j].r]++;
-                srt[omp_get_thread_num()][pixels[i][j].g]++;
-                srt[omp_get_thread_num()][pixels[i][j].b]++;
+                srt[pixels[i][j].r]++;
+                srt[pixels[i][j].g]++;
+                srt[pixels[i][j].b]++;
+            }
+        }
+
+        int pxls = 0;
+        for (int i = 0; i <= 255; i++) {
+            pxls += srt[i];
+            if ((double) pxls > (double) sz * k) {
+                mn = i;
+                break;
+            }
+        }
+
+        pxls = 0;
+        for (int i = 255; i >= 0; i--) {
+            pxls += srt[i];
+            if ((double) pxls > (double) sz * k) {
+                mx = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                pixels[i][j].r = f(pixels[i][j].r);
+                pixels[i][j].g = f(pixels[i][j].g);
+                pixels[i][j].b = f(pixels[i][j].b);
+            }
+        }
+
+    }
+    else {
+
+        vector <vector <int>> srt (flows, vector <int> (SZ));
+
+        omp_set_num_threads(flows);
+        #pragma omp parallel
+        {
+            #pragma omp for schedule(KIND, CHUNK_SIZE)
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    srt[omp_get_thread_num()][pixels[i][j].r]++;
+                    srt[omp_get_thread_num()][pixels[i][j].g]++;
+                    srt[omp_get_thread_num()][pixels[i][j].b]++;
+                }
+            }
+        }
+
+        
+        int pxls = 0;
+        for (int i = 0; i <= 255; i++) {
+            for (int flow = 0; flow < flows; flow++) {
+                pxls += srt[flow][i];
+            }
+            if ((double) pxls > (double) sz * k) {
+                mn = i;
+                break;
+            }
+        }
+
+        pxls = 0;
+        for (int i = 255; i >= 0; i--) {
+            for (int flow = 0; flow < flows; flow++) pxls += srt[flow][i];
+            if ((double) pxls > (double) sz * k) {
+                mx = i;
+                break;
+            }
+        }
+
+        
+
+        omp_set_num_threads(flows);
+        #pragma omp parallel
+        {
+            #pragma omp for schedule(KIND, CHUNK_SIZE)
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    pixels[i][j].r = f(pixels[i][j].r);
+                    pixels[i][j].g = f(pixels[i][j].g);
+                    pixels[i][j].b = f(pixels[i][j].b);
+                }
             }
         }
     }
@@ -270,44 +458,14 @@ void plus_P6 (FILE* fin, FILE* fout, double k) {
     cerr << "Time (" << flows << " thread(s)): ";
     cerr << chrono::duration_cast<chrono::milliseconds>(end - begin).count() << " ms\n";
 
-    int sz = 3 * h * w;
-    unsigned char mn = 255;
-    unsigned char mx = 0;
-    
-    int pxls = 0;
-    for (int i = 0; i <= 255; i++) {
-        for (int flow = 0; flow < flows; flow++) {
-            pxls += srt[flow][i];
-        }
-        if ((double) pxls > (double) sz * k) {
-            mn = i;
-            break;
-        }
-    }
-
-    pxls = 0;
-    for (int i = 255; i >= 0; i--) {
-        for (int flow = 0; flow < flows; flow++) pxls += srt[flow][i];
-        if ((double) pxls > (double) sz * k) {
-            mx = i;
-            break;
-        }
-    }
-
-    auto f = [&] (unsigned char x) {
-        int pxl = (int(x - mn) * 255 / int(mx - mn));
-        if (pxl < 0) return (unsigned char) 0;
-        if (pxl > 255) return (unsigned char) 255;
-        return (unsigned char)(int(x - mn) * 255 / int(mx - mn));
-    };
-
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
-            fprintf(fout, "%c", f(pixels[i][j].r));
-            fprintf(fout, "%c", f(pixels[i][j].g));
-            fprintf(fout, "%c", f(pixels[i][j].b));
+            fprintf(fout, "%c", pixels[i][j].r);
+            fprintf(fout, "%c", pixels[i][j].g);
+            fprintf(fout, "%c", pixels[i][j].b);
         }
     }
+
 }
 
 
